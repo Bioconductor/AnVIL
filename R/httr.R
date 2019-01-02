3a7950e9cc9ac1ee7869e05ea8a66231db4a9d0c
Nitesh Turaga <nitesh.turaga@gmail.com>
2019-01-02 11:37:25 -0500

    Add .put functionality to httr.R
    This allows to implement all the PUT functions in API. There are
    somethings which need to be checked, i.e enabling user to give 'body' in
    more than just json.

--- R/httr.R
+++ R/httr.R
@@ -27,10 +27,38 @@
     } else response
 }
 
+
+## example PUT call,
+## PUT(url, anvil_options("leonardo_config"), body = list(biocImage =
+## "us.gcr.io/anvil-leo-dev/anvil_bioc_docker:latest"), encode="json",
+## config(token=x))
+
 .put <-
-    function()
+    function(path, authorization = NULL, body = NULL, verbose = FALSE,
+             content_only = TRUE, check = stop_for_status)
 {
+    ## Validate args
+    stopifnot(
+        .is_scalar_logical(verbose),
+        .is_scalar_logical(content_only)
+    )
+
+    ## compose request
+    url <- paste0(anvil_options("leonardo_host"), path)
+
+    ## PUT and check response
+    response <- PUT(
+        url, anvil_options("leonardo_config"), body = body,
+        encode = "json", authorization, if (verbose) verbose()
+    )
+    check(response)
+
+    ## Process and return result
+    if (content_only) {
+        content(response, "text")
+    } else response
 }
+
 
 .post <-
     function()
