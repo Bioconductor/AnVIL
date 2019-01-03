6a34f030fed49676d17daf37ff8033d4461f917a
Nitesh Turaga <nitesh.turaga@gmail.com>
2019-01-03 11:28:09 -0500

    Add functions for remaining API functionality in leonardo.
    - cluster delete,
    - cluster update,
    - cluster start,
    - cluster stop,
    - notebooks api localize

--- R/leonardo.R
+++ R/leonardo.R
@@ -15,7 +15,7 @@
 #'     api, documented at `leonardo.dev.anvilproject.org/#/`. See
 #'     `?authenticate` for OAuth 2.0 authentication.
 #'
-#' @details `api_clusters()` lists all active clusters. 
+#' @details `api_clusters()` lists all active clusters.
 #'
 #' @param googleProject (optional) character(1) name of google
 #'     project, e.g., `"anvil-leo-dev"`
@@ -83,7 +83,7 @@ api_clusters <-
 #'
 #' @return `api_cluster()` returns a list-of-lists describing a single
 #'     cluster.
-#' 
+#'
 #' @export
 api_cluster <-
     function(googleProject = "anvil-leo-dev", clusterName, verbose = FALSE)
@@ -99,6 +99,73 @@ api_cluster <-
     fromJSON(response)
 }
 
+
+#' @rdname leonardo
+#'
+#' @details `api_update_cluster()` updates the configuration of a
+#'     cluster. In order to update the configuration of a cluster, it
+#'     must first be running, check with `api_clustes()`.
+#'
+#' @param googleProject character(1) name of google project, e.g,
+#'     `"anvil-leo-dev"`
+#'
+#' @param clusterName character(1) name of a cluster to query for
+#'     details. Names are from `api_clusters()$labels.clusterName)`.
+#'
+#' @param updateClusterRequest list(1) request to a cluster resource in the
+#'     form of list(requestName = updateClusterRequest).
+#'     Eg: list("machineConfig" = list("numberOfWorkers" = 0,
+#'                                     "numberOfPreemptibleWorkers" = 0),
+#'              "autopause" = true,
+#'              "autopauseThreshold" = 0)
+#'
+#' @return `api_create_cluster()` returns a json list-of-lists
+#'     describing the updated cluster.
+#'
+#' @export
+api_update_cluster <-
+    function(googleProject, clusterName, updateClusterRequest)
+{
+    stopifnot(
+        .is_scalar_character(googleProject),
+        .is_scalar_character(clusterName),
+        is.list(updateClusterRequest)
+    )
+
+    path <- sprintf("/api/cluster/%s/%s", googleProject, clusterName)
+    token <- authenticate()
+    response <- .patch(path, config(token = token), body = updateClusterRequest,
+                       verbose = verbose)
+    fromJSON(response)
+}
+
+#' @rdname leonardo
+#'
+#' @details `api_delete_cluster()` deletes an existing Dataproc
+#'     cluster in the given project
+#'
+#' @param googleProject character(1) name of google project, e.g,
+#'     `"anvil-leo-dev"`
+#'
+#' @param clusterName character(1) name of a cluster to query for
+#'     details. Names are from `api_clusters()$labels.clusterName)`.
+#'
+#' @return character(1) response body is a string.
+#'
+#' @export
+api_delete_cluster <-
+    function(googleProject, clusterName)
+{
+    stopifnot(
+        .is_scalar_character(googleProject),
+        .is_scalar_character(clusterName)
+    )
+
+    path <- sprintf("/api/cluster/%s/%s", googleProject, clusterName)
+    token <- authenticate()
+    response <- .delete(path, config(token = token), verbose = verbose)
+    response
+}
 
 ##
 ## This implements only the v2 of the API which creates the dataproc
@@ -146,6 +213,63 @@ api_create_cluster <-
 }
 
 
+#' @rdname leonardo
+#'
+#' @details `api_cluster_start()` starts a Dataproc cluster
+#'
+#' @param googleProject character(1) name of google project, e.g,
+#'     `"anvil-leo-dev"`
+#'
+#' @param clusterName character(1) name of a cluster to query for
+#'     details. Names are from `api_clusters()$labels.clusterName)`.
+#'
+#'
+#' @return
+#'
+#' @export
+api_cluster_start <-
+    function(googleProject, clusterName, verbose = FALSE)
+{
+    stopifnot(
+        .is_scalar_character(googleProject),
+        .is_scalar_character(clusterName)
+    )
+
+    path <- sprintf("/api/cluster/%s/%s/start", googleProject, clusterName)
+    token <- authenticate()
+    response <- .post(path, config(token = token), verbose = verbose)
+    fromJSON(response)
+}
+
+
+#' @rdname leonardo
+#'
+#' @details `api_cluster_stop()` stops a Dataproc cluster .The
+#'     cluster may be restarted with the `api_cluster_start()` endpoint
+#'
+#' @param googleProject character(1) name of google project, e.g,
+#'     `"anvil-leo-dev"`
+#'
+#' @param clusterName character(1) name of a cluster to query for
+#'     details. Names are from `api_clusters()$labels.clusterName)`.
+#'
+#' @return
+#'
+#' @export
+api_cluster_stop <-
+    function(googleProject, clusterName, verbose = FALSE)
+{
+    stopifnot(
+        .is_scalar_character(googleProject),
+        .is_scalar_character(clusterName)
+    )
+
+    path <- sprintf("/api/cluster/%s/%s/stop", googleProject, clusterName)
+    token <- authenticate()
+    response <- .post(path, config(token = token), verbose = verbose)
+    fromJSON(response)
+}
+
 
 ##
 ## notebooks
@@ -166,14 +290,61 @@ api_create_cluster <-
 #'
 #' @export
 notebooks_cluster_name <-
-    function(googleProject, clusterName)
+    function(googleProject, clusterName, verbose = FALSE)
 {
     path <- sprintf("/notebooks/%s/%s", googleProject, clusterName)
     token <- authenticate()
-    response <- .get(path, config(token=token), verbose)
+    response <- .get(path, config(token=token), verbose = verbose)
     fromJSON(response)
 }
 
+
+#' @rdname leonardo
+#'
+#' @details `notebooks_api_localize()` localize files to/from a
+#'     Jupyter notebook server.Sends a command to a Jupyter notebook
+#'     server to localize files to/from the server. Supports GCS paths
+#'     and Data URIs. Output, including any errors, will appear in
+#'     localization.log in the working directory of the Jupyter
+#'     notebook server. By default this operation will happen
+#'     synchronously and the response status will reflect any errors
+#'     encountered in the copy. However, if the async parameter is
+#'     specfied then the localization will happen asynchronously to
+#'     the request, and the API will always return 200.
+#'
+#' @param googleProject character(1) name of google project, e.g,
+#'     `"anvil-leo-dev"`
+#'
+#' @param clusterName character(1) name of a cluster to query for
+#'     details. Names are from `api_clusters()$labels.clusterName)`.
+#'
+#' @param async logical(1) if TRUE, the copy will happen
+#'     asynchronously to the request and the API will always return
+#'     200. If FALSE (the default), the copy will happen synchronously
+#'     and the response will reflect any errors encountered during the
+#'     copy.
+#'
+#' @param filesToLocalize list(1)
+#'
+#' @return
+#'
+#' @export
+notebooks_api_localize <-
+    function(googleProject, clusterName, async = FALSE, filesToLocalize)
+{
+    stopifnot(
+        .is_scalar_character(googleProject),
+        .is_scalar_character(clusterName),
+        is.logical(async),
+        is.list(filesToLocalize)
+    )
+
+    path <- sprintf("/notebooks/%s/%s/api/localize", googleProject, clusterName)
+    token <- authenticate()
+    response <- .post(path, config(token = token), body = filesToLocalize,
+                      verbose = verbose, query = async)
+    fromJSON(response)
+}
 
 
 #' @rdname leonardo
@@ -219,9 +390,8 @@ notebooks_invalidate_token <-
 {
     path  <- "/notebooks/invalidateToken"
     token <- authenticate()
-    .get(path, config(token = token), verbose)
+    .get(path, config(token = token), verbose = verbose)
 }
-
 
 
 ##
@@ -245,10 +415,10 @@ status <-
     fromJSON(response)
 }
 
+
 ##
 ## test
 ##
-
 
 #' @rdname leonardo
 #'
