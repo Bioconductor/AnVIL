b14c8378881cb0105f1e17375c8e528170063e6d
Martin Morgan <martin.morgan@roswellpark.org>
2019-06-17 05:58:21 -0400

    add zchar support to .is_character_0_or_1

--- R/utilities.R
+++ R/utilities.R
@@ -3,8 +3,12 @@
         (na.ok || !is.na(x)) &&
         (zchar || nzchar(x))
 
-.is_character_0_or_1 <- function(x, na.ok = FALSE)
-    (length(x) == 0L && is.character(x)) || .is_scalar_character(x, na.ok)
+.is_character_0_or_1 <-
+    function(x, na.ok = FALSE, zchar = FALSE)
+{
+    (length(x) == 0L && is.character(x)) ||
+        .is_scalar_character(x, na.ok, zchar)
+}
 
 .is_scalar_logical <- function(x, na.ok = FALSE)
     is.logical(x) && length(x) == 1L && (na.ok || !is.na(x))
