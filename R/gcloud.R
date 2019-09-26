4419788260b421e98732180693b470aff46dd533
Martin Morgan <martin.morgan@roswellpark.org>
2019-09-26 18:16:22 -0400

    (exported) gcloud_*() helper functions

--- R/gcloud.R
+++ R/gcloud.R
@@ -4,8 +4,18 @@
     .gcloud_sdk_do("gcloud", c(...))
 }        
 
+#' @export
 gcloud_account <- function()
     .gcloud_do("config", "get-value", "account")
 
+#' @export
 gcloud_project <- function()
     .gcloud_do("config", "get-value", "project")
+
+#' @export
+gcloud_help <- function(...)
+    noquote(.gcloud_do("help", ...))
+
+#' @export
+gcloud_cmd <- function(cmd, ...)
+    .gcloud_do(cmd, ...)
