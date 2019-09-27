63bff8820fef2d06c7fc73b4c35625900a9655f8
Martin Morgan <martin.morgan@roswellpark.org>
2019-09-26 20:38:08 -0400

    add access token to Leonardo

--- R/Leonardo.R
+++ R/Leonardo.R
@@ -1,3 +1,10 @@
+#' @export
+.Leonardo <- setClass(
+    "Leonardo",
+    contains = "Service",
+    slots = c(api_header = "character")
+)
+
 ## construct a singleton instance for this service
 
 #' @rdname Services
@@ -11,10 +18,22 @@
 Leonardo <-
     function()
 {
-    Service(
-        "leonardo",
-        host = "notebooks.firecloud.org",
-        config = httr::config(ssl_verifypeer = 0L, ssl_verifyhost = 0L),
-        api_url = "https://notebooks.firecloud.org/api-docs.yaml"
+    .Leonardo(
+        Service(
+            "leonardo",
+            host = "notebooks.firecloud.org",
+            config = httr::config(ssl_verifypeer = 0L, ssl_verifyhost = 0L),
+            api_url = "https://notebooks.firecloud.org/api-docs.yaml",
+            authenticate = FALSE
+        ),
+        api_header = api_header
     )
 }
+
+#' @export
+setMethod(
+    "operations", "Leonardo",
+    function(x)
+{
+    callNextMethod(x, .headers = .api_header(x))
+})
