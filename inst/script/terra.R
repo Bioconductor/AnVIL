6e60e4a3afeaaf212d27e5c45b345b930e057bcf
Martin Morgan <Martin.Morgan@RoswellPark.org>
2020-04-27 12:48:32 -0400

    remove old scripts and version bump

--- inst/script/terra.R
+++
@@ -1,7 +0,0 @@
-devtools::load_all()
-library(dplyr)
-
-namespace="fccredits-indium-azure-9792"
-workspace="TCGA_LUSC_hg38_OpenAccess_GDCDR-12-0_DATA_copy"
-
-terra$getEntityTypes(namespace, workspace) %>% str()
