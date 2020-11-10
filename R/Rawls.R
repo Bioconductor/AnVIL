859d87f444079d7292ec617515c046ba5334b06a
Martin Morgan <Martin.Morgan@RoswellPark.org>
2020-11-10 17:51:48 -0500

    add Rawls() service

---
+++ R/Rawls.R
@@ -0,0 +1,53 @@
+#' @export
+.Rawls <- setClass(
+    "Rawls",
+    contains = "Service",
+    slots = c(api_header = "character")
+)
+
+## construct a singleton instance for this service
+
+#' @rdname Services
+#'
+#' @aliases Rawls-class operations,Rawls-method schemas,Rawls-method
+#'
+#' @return `Rawls()` creates the API of the Rawls cloud computational
+#'     environemnt at https://rawls.dsde-prod.broadinstitute.org.
+#'
+#' @format NULL
+#'
+#' @examples
+#' if (gcloud_exists()) {
+#'     rawls <- Rawls()
+#'     tags(rawls)
+#'     tags(rawls, "billing")
+#' }
+#'
+#' @export
+Rawls <-
+    function()
+{
+    access_token <- .gcloud_access_token()
+    api_header <- c(Authorization = paste("Bearer", access_token))
+    .Rawls(
+        Service(
+            "rawls",
+            host = "rawls.dsde-prod.broadinstitute.org",
+            authenticate = FALSE,
+            api_reference_url = "https://rawls.dsde-prod.broadinstitute.org/api-docs.yaml"
+        ),
+        api_header = api_header
+    )
+}
+
+## Some operations seem to have a poorly-defined operationId in the json
+
+#' @export
+setMethod(
+    "operations", "Rawls",
+    function(x, ..., .deprecated = FALSE)
+{
+    callNextMethod(
+        x, .headers = .api_header(x), ..., .deprecated = .deprecated
+    )
+})
--- tests/testthat/test_Services.R
+++ tests/testthat/test_Services.R
@@ -2,5 +2,6 @@ test_that("Services are current", {
     skip_if(!gcloud_exists())
     expect_silent(Terra())
     expect_silent(Leonardo())
+    expect_silent(Rawls())
     expect_silent(Dockstore())
 })
