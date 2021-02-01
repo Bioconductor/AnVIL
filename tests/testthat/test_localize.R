5f8d1ae0cc8f4b15ef874085a974a9935fe2fef7
Martin Morgan <Martin.Morgan@RoswellPark.org>
2021-02-01 16:47:35 -0500

    update unit test; add missing man page; version bump

--- tests/testthat/test_localize.R
+++ tests/testthat/test_localize.R
@@ -6,13 +6,9 @@ test_that("repositories() works", {
     object <- withr::with_envvar(envvars, repositories("3.10"))
     expect_identical(object, BiocManager::repositories())
 
-    envvars <- c(
-        TERRA_R_PLATFORM = "anvil-rstudio-bioconductor",
-        TERRA_R_PLATFORM_BINARY_VERSION = "0.99.1"
-    )
-    object <- withr::with_envvar(envvars, repositories("3.10"))
+    object <- repositories("3.13")
     expected <- c(
-        "https://storage.googleapis.com/anvil-rstudio-bioconductor/0.99/3.10",
+        "https://storage.googleapis.com/bioconductor_docker/packages/3.13/bioc",
         BiocManager::repositories()
     )
     expect_identical(object, expected)
