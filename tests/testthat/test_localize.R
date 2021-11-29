c06a83dea16d54404152219a0be8b78478de5e3f
Martin Morgan <Martin.Morgan@RoswellPark.org>
2021-11-29 14:36:15 -0500

    version bump and unit test update

--- tests/testthat/test_localize.R
+++ tests/testthat/test_localize.R
@@ -8,7 +8,8 @@ test_that("repositories() works", {
     envvars <- c(BIOCONDUCTOR_DOCKER_VERSION = "3.13.0")
     object <- withr::with_envvar(envvars, repositories("3.13"))
     expected <- c(
-        BiocBinaries = "https://storage.googleapis.com/bioconductor_docker/packages/3.13/bioc",
+        BiocBinaries =
+            "https://bioconductor.org/packages/3.13/container-binaries",
         BiocManager::repositories()
     )
     expect_identical(object, expected)
