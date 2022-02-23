4a1849f2ddfd0fd51860c18c6e4d0227c0ce39a1
Martin Morgan <Martin.Morgan@RoswellPark.org>
2022-02-23 16:16:21 -0500

    update unit test to new repository location

--- tests/testthat/test_localize.R
+++ tests/testthat/test_localize.R
@@ -5,11 +5,11 @@ test_that("repositories() works", {
     expect_identical(object, BiocManager::repositories())
 
     ## existing repository
-    envvars <- c(BIOCONDUCTOR_DOCKER_VERSION = "3.13.0")
-    object <- withr::with_envvar(envvars, repositories("3.13"))
+    envvars <- c(BIOCONDUCTOR_DOCKER_VERSION = "3.14.0")
+    object <- withr::with_envvar(envvars, repositories("3.14"))
     expected <- c(
         BiocBinaries =
-            "https://bioconductor.org/packages/3.13/container-binaries",
+            "https://bioconductor.org/packages/3.14/container-binaries",
         BiocManager::repositories()
     )
     expect_identical(object, expected)
