3c16f464c7c57b2c1fcb6b62ad662981c5cde7a5
Martin Morgan <Martin.Morgan@RoswellPark.org>
2022-09-21 13:34:15 -0400

    add infrastructure and initail snapshot for tracking API changes
    - See 'Test endpoints' section of vignettes/Introduction.Rmd for
      details

--- R/api.R
+++ R/api.R
@@ -142,7 +142,6 @@
     )
 {
     operation_defs <- get_operation_definitions(api, path)
-
     lapply(operation_defs, function(op_def){
         what <- toupper(op_def$action)
         if (!what %in% c("POST", "PATCH", "PUT", "GET", "HEAD", "DELETE"))
@@ -196,4 +195,62 @@
         class(FUN) <- c(.CLASS_OPERATION, class(FUN))
         FUN
     })
+}
+
+## The following functions are for use by tests/testthat/test_api.R
+
+.api_test_file_path <-
+    function(name)
+{
+    devtools::package_file("tests", "testthat", paste0("api-", name, ".rds"))
+}
+
+.api_test_in_use <- function(function_names, service_name, r_content) {
+    in_use <- vapply(
+        function_names, function(function_name, service_name, r_content) {
+            ## FIXME: fragile
+            function_call <- paste0(service_name, "()$", function_name, "(")
+            any(grepl(function_call, r_content, fixed = TRUE))
+        }, logical(1), service_name, r_content)
+    function_names[in_use]
+}
+
+.api_test_write <-
+    function(service, name)
+{
+    file_path <- .api_test_file_path(name)
+    ops <- lapply(operations(service), formals)
+    saveRDS(ops, file_path)
+}
+
+.api_test_check <-
+    function(service, name)
+{
+    ops <- lapply(operations(service), formals)
+    ops_saved <- readRDS(.api_test_file_path(name))
+
+    common <- intersect(names(ops), names(ops_saved))
+    added <- setdiff(names(ops), names(ops_saved))
+    removed <- setdiff(names(ops_saved), names(ops))
+
+    is_updated <- vapply(common, function(op_name) {
+        !identical(ops_saved[[op_name]], ops[[op_name]])
+    }, logical(1))
+    updated <- common[is_updated]
+
+    r_files <- dir(
+        devtools::package_file("R"), full.names = TRUE, pattern = ".R$"
+    )
+    r_content <- unlist(lapply(r_files, readLines))
+
+    common_in_use <- .api_test_in_use(common, name, r_content)
+    removed_in_use <- .api_test_in_use(removed, name, r_content)
+    updated_in_use <- .api_test_in_use(updated, name, r_content)
+
+    list(
+        common = common, added = added, removed = removed, updated = updated,
+        common_in_use = common_in_use,
+        removed_in_use = removed_in_use,
+        updated_in_use = updated_in_use
+    )
 }
--- tests/testthat/test_api.R
+++ tests/testthat/test_api.R
@@ -24,3 +24,28 @@ test_that("'accept' defaults to */*", {
     op_def <- list(produces = produces)
     expect_identical(.api_get_accept(op_def), httr::accept(produces))
 })
+
+
+## Check that the API calls used by AnVIL are consistent with the API
+## in the YAML. Requires manual investigation of any removed or
+## updated_args_in_use functions.
+##
+## Use functionality in R/api.R:.api_test_write() to record the
+## current interface; .api_test_check() to compare the current and
+## previously recorded versions.
+
+test_that("Interfaces are current", {
+    skip_if(!gcloud_exists())
+
+    service_status <- .api_test_check(Terra(), "Terra")
+    expect_identical(service_status$removed_in_use, character())
+    expect_identical(service_status$updated_in_use, character())
+
+    service_status <- .api_test_check(Rawls(), "Rawls")
+    expect_identical(service_status$removed_in_use, character())
+    expect_identical(service_status$updated_in_use, character())
+
+    service_status <- .api_test_check(Leonardo(), "Leonardo")
+    expect_identical(service_status$removed_in_use, character())
+    expect_identical(service_status$updated_in_use, character())
+})
