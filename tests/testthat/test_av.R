a9ff27513a24d48b6ce0ddee06ff335f15f2b82f
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-09-01 15:20:30 -0400

    isolate .avworkspaces_clean and test

--- tests/testthat/test_av.R
+++ tests/testthat/test_av.R
@@ -116,3 +116,28 @@ test_that(".avbucket_path() trims arguments correctly", {
         .avbucket_path("gs://foo", "bar", c("baz", "bing"))
     )
 })
+
+test_that(".av_workspaces_clean() preserves leading and trailing spaces", {
+    ## add intentional spaces in workspace.name
+    testws <- tibble::tibble(
+        workspace.name = " a test workspace ",
+        workspace.lastModified = "2023-06-26T21:17:26.343Z",
+        workspace.createdBy = "test.bioc@gmail.com",
+        workspace.namespace = "bioconductor-rpci-anvil",
+        accessLevel = "READER",
+        extraCol = TRUE
+    )
+    restbl <- testws
+    ## for testing names
+    coi <- c("name", "lastModified", "createdBy", "namespace", "accessLevel")
+    names(restbl) <- coi
+    ## for testing select operation
+    restbl <- restbl[, coi]
+    ## for testing Date coercion
+    restbl[["lastModified"]] <- as.Date(restbl[["lastModified"]])
+
+    expect_identical(
+        .avworkspaces_clean(testws),
+        restbl
+    )
+})
