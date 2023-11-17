681f8a35dfa98b42a39e7a950cf0b7684c8cc1fd
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-11-17 21:41:05 +0000

    move .avstop_for_status

--- R/av.R
+++ R/av.R
@@ -6,40 +6,6 @@
 NULL
 
 ##
-## internal
-##
-
-#' @importFrom httr status_code http_condition headers
-.avstop_for_status <-
-    function(response, op)
-{
-    status <- status_code(response)
-    if (status < 400L)
-        return(invisible(response))
-
-    cond <- http_condition(status, "error")
-    type <- headers(response)[["content-type"]]
-    msg <- NULL
-    if (nzchar(type) && grepl("application/json", type)) {
-        content <- as.list(response)
-        msg <- content[["message"]]
-        if (is.null(msg))
-            ## e.g., from bond DRS server
-            msg <- content$response$text
-    } else if (nzchar(type) && grepl("text/html", type)) {
-        ## these pages can be too long for a standard 'stop()' message
-        cat(as.character(response), file = stderr())
-    }
-
-    message <- paste0(
-        "'", op, "' failed:\n  ",
-        conditionMessage(cond),
-        if (!is.null(msg)) "\n  ", msg
-    )
-    stop(message, call.=FALSE)
-}
-
-##
 ## tables
 ##
 
