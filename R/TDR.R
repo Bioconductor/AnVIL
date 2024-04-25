758430fbd1fcddc504c101ff93dd5d022b5118ce
LiNk-NY <marcel.ramosperez@roswellpark.org>
2024-04-25 17:03:09 -0400

    add Terra Data Repository service

---
+++ R/TDR.R
@@ -0,0 +1,57 @@
+#' @exportClass TDR
+.TDR <- setClass(
+    "TDR",
+    contains = "Service",
+    slots = c(api_header = "character")
+)
+
+.api_header <- function(x) x@api_header
+.TDR_API_REFERENCE_VERSION <- "0.1.0"
+
+#' @rdname Services
+#'
+#' @aliases TDR-class operations,TDR-method
+#'
+#' @return `TDR()` creates the API of the Terra Data Repository to work with
+#'   snapshot data in the Terra Data Repository at \url{data.terra.bio}.
+#'
+#' @format NULL
+#'
+#' @examples
+#'
+#' TDR()
+#'
+#' @export
+TDR <-
+    function()
+{
+    api_header <- character()
+    path <- authenticate_path("tdr")
+    if (file.exists(path)) {
+        token <- read_json(path)$token
+        api_header <- c(Authorization = paste("Bearer", token))
+    }
+    .TDR(
+        Service(
+            "tdr",
+            host = "data.terra.bio",
+            config = httr::config(ssl_verifypeer = 0L, ssl_verifyhost = 0L),
+            api_reference_version = .TDR_API_REFERENCE_VERSION,
+            authenticate = FALSE,
+            api_reference_url =
+                "https://data.terra.bio/data-repository-openapi.yaml"
+        ),
+        api_header = api_header
+    )
+}
+
+
+#' @export
+setMethod(
+    "operations", "TDR",
+    function(x, ..., .deprecated = FALSE)
+{
+    callNextMethod(
+        x, .headers = .api_header(x), ..., .deprecated = .deprecated
+    )
+})
