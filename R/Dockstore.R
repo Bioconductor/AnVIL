98d43a7be2ce212567c46f48122c367a444e0606
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-01-24 09:59:38 -0500

    update version in Dockstore.R

--- R/Dockstore.R
+++ R/Dockstore.R
@@ -8,7 +8,7 @@
 )
 
 .api_header <- function(x) x@api_header
-.DOCKSTORE_API_REFERENCE_VERSION <- "1.16.3"
+.DOCKSTORE_API_REFERENCE_VERSION <- "1.16.5"
 
 ## construct a singleton instance for this service
 
