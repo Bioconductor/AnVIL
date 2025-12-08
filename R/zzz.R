b62b265262beeacf1b9b76de2b07b32e8927bbcd
LiNk-NY <marcel.ramos@roswellpark.org>
2025-12-08 21:57:35 +0000

    keep text within 80 char width

--- R/zzz.R
+++ R/zzz.R
@@ -4,7 +4,13 @@
     function(...)
 {
     opts <- list(
-        anvil_client_id = "250475993726-k70p3kf2fe2tpuq5jn39ogafbuj9fb8o.apps.googleusercontent.com",
+        anvil_client_id = paste(
+            "250475993726-k70p3kf2fe2tpuq5jn39ogafbuj9fb8o",
+            "apps",
+            "googleusercontent",
+            "com",
+            sep = "."
+        ),
         anvil_client_secret = oauth_secret
     )
     opts <- opts[!names(opts) %in% names(options())]
