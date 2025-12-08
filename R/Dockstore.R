33b6b1a89b91f0ed217cc014d84eee75b3630e00
LiNk-NY <LiNk-NY@users.noreply.github.com>
2025-12-08 17:18:12 +0000

    Update API

--- R/Dockstore.R
+++ R/Dockstore.R
@@ -8,7 +8,7 @@
 )
 
 .api_header <- function(x) x@api_header
-.DOCKSTORE_API_REFERENCE_VERSION <- "1.18.0"
+.DOCKSTORE_API_REFERENCE_VERSION <- "1.18.2"
 
 ## construct a singleton instance for this service
 
